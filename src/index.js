cb0d690b24eb651e1e070aa46d8cc091c2409eaf
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-21 14:29:55 +0300

    split

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  // const solutions = solveEquation('294 * x^2 - 141195558 * x - 1600964090384736');
+var equationSplitted = equation.split(' ');
+var a = equationSplitted[0];
+var b = equationSplitted[4];
+var c = equationSplitted[8];
+alert[c];
 }
+
