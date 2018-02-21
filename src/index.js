dee69b687191659cd0aefe78c3b64b64a6714058
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-21 14:54:46 +0300

    ready for test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ module.exports = function solveEquation(equation) {
   // your implementation
   // const solutions = solveEquation('294 * x^2 - 141195558 * x - 1600964090384736');
 var equationSplitted = equation.split(' ');
-var a = equationSplitted[0];
-var b = equationSplitted[4];
-var c = equationSplitted[8];
+var a,b,c;
+a = equationSplitted[0];
+if(equationSplitted[3] == '+'){
+b = equationSplitted[4];
+}
+else {
+b = (0-(equationSplitted[4]))
+}
+
+if(equationSplitted[7] == '+'){
+c = equationSplitted[8];
+}
+else {
+c = (0-(equationSplitted[8]))
+}
+var d = (b * b) - 4 * a * c;
+
+var dSqrt, xPlus, xMinus;
+
+dSqrt = Math.sqrt(d);
+
+xPlus = ((-b) + dSqrt) / (2 * a);
+xMinus = ((-b) - dSqrt) / (2 * a);
+
+var result = [];
+
+result.push(xPlus,xMinus);
+return result;
 
 }
 
